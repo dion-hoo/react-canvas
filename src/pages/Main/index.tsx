7070548beefae13476b003fdbe8dc48621669c8e
Dion <dion.hoooo@gmail.com>
2023-08-26 20:14:00 +0900

    eslint 에러나는 부분 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,7 @@
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+import { PATH } from '@/urls/url';
 
 import styles from './index.module.scss';
 
@@ -8,9 +11,20 @@ const Main = () => {
   return (
     <div className={cx('wrap')}>
       <h1>Main</h1>
-      <ul>
-        <li>1</li>
-      </ul>
+
+      <section className={cx('content')}>
+        <ul>
+          <li>
+            <Link to={PATH.CONVEXHULL}>1. ConvexHull</Link>
+          </li>
+          <li>
+            <Link to={PATH.METABALL}>2. MetaBall</Link>
+          </li>
+          <li>
+            <Link to={PATH.NEWSTICKER}>3. Sticker</Link>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
